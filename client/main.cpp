8daf5b9cf1133ad025ed5032f279f0bd64480f90
Antoine MALGAT <antoine.malgat@epitech.eu>
2026-04-01 17:30:27 +0200

    feat: simple connection to server

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,40 @@
-int main(void) {
-    return 0;
+
+#include "Socket.hpp"
+#include <cstdint>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <netinet/in.h>
+#include <span>
+#include <string>
+#include <string_view>
+#include <utility>
+
+std::unique_ptr<utils::Socket> createSocket(std::string adress,
+                                            std::string port) {
+  auto socket =
+      std::make_unique<utils::Socket>(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  socket->connect(adress, std::stoi(port));
+  return socket;
+}
+
+std::int32_t main(std::int32_t ac, char **av) {
+
+  if (ac != 3) {
+    std::cout
+        << "USAGE: ./myteams_cli ip port\n"
+        << "\tip is the server ip address on which the server socket listens\n"
+        << "\tport is the port number on which the server socket listens\n";
+    if (ac == 2 && std::string_view(av[1]) == "--help") {
+      return 0;
+    }
+    return 84;
+  }
+
+  try {
+    auto socket = createSocket(av[1], av[2]);
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 84;
+  }
 }
