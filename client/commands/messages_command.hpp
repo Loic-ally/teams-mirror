a183b7e5863a48c326e7f50163bea3e2b18c3b63
Lolo <loic.philippe@epitech.eu>
2026-04-12 22:07:30 +0200

    feat: fixed include guards

--- client/commands/messages_command.hpp
+++ client/commands/messages_command.hpp
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef CLIENT_COMMANDS_MESSAGE_COMMAND_HPP
+#define CLIENT_COMMANDS_MESSAGE_COMMAND_HPP
 
+#ifdef _WIN32
+#pragma once
+#endif
 namespace client {
 class Client;
 class ParsedInput;
@@ -10,3 +14,5 @@ namespace client::commands {
 void handleMessages(Client &clientData, ParsedInput &input);
 
 } // namespace client::commands
+
+#endif //CLIENT_COMMANDS_MESSAGE_COMMAND_HPP
